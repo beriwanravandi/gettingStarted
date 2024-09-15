cc3df118063a88e115eb72cbeaed86957c167f07
beriwanravandi <bs4817@nyu.edu>
2024-09-14 21:13:24 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -16,13 +16,13 @@ def welcome_assignment_answers(question):
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-        answer = "7b28e50aa1e738dd0c22b1b51251c89de24a40fbdd5704593754bbdf9d9fb0f3  -"
+        answer = "7b28e50aa1e738dd0c22b1b51251c89de24a40fbdd5704593754bbdf9d9fb0f3"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = 5
+        answer = int(5)
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = 3
+        answer = int(3)
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
